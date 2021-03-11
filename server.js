4a48664b48e371a7d64676e1d6f1a16557356a74
Lebeil <liedel75@gmail.com>
2021-03-11 18:17:51 +0100

    first commit

---
+++ server.js
@@ -0,0 +1,9 @@
+let express = require('express');
+let app = express();
+
+app.use(express.static(__dirname + '/public'));
+
+let port = 8000;
+
+app.listen(port);
+console.log('server on ' + port);
